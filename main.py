7ca2a85a17900f357f113a28325219abbd8e53de
GGRaai <raimundomoraga@hotmail.com>
2020-05-17 19:39:50 -0400

    funciones de formato funcionando ~70%, mega no funciona.

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import noticias
+import test
+
+url = "https://www.biobiochile.cl/noticias/nacional/chile/2020/05/17/minsal-confirma-ministros-ward-briones-estan-cuerentena-preventiva-explica-caso-sichel.shtml"
 
 
-e = noticias.Emol("Pencazo",2,3,4,5,6,7,8)
-print(e.autor)
-m = noticias.Bio_Bio("Pencazo",2,3,4,5,6,7,"visitas")
-print(m.visitas)
+if __name__ == "__main__":
+    if('emol' in url):
+        test.formatear_emol(url)
+    elif('meganoticias' in url):
+        test.formatear_mega(url)
+    elif('biobiochile' in url):
+        test.formatear_bio_bio(url)
+    elif('latercera' in url):
+        test.formatear_la_tercera(url)
--- test.py
+++ test.py
@@ -2,8 +2,9 @@ import requests
 import pprint
 from bs4 import BeautifulSoup
 
-url = "https://www.emol.com/noticias/Nacional/2020/05/16/986388/Manalich-senador-Quinteros-coronavirus-dolo.html"
 def formatear_emol(url):
+    #TODO:
+    #   -Arreglar cuerpo
     pagina=dict()
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -41,12 +42,12 @@ def formatear_emol(url):
     print(pagina)
     return pagina
 
-def formatear_mega(url):
+def formatear_mega(url): #no funciona
     pagina=dict()
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     #Sacar datos
-    titulo = soup.find(id="cuDetalle_cuTitular_tituloNoticia")
+    titulo = soup.find('h1')
     subtitulo = soup.find(id="cuDetalle_cuTitular_bajadaNoticia")
     datos= soup.find(id="barra-agencias-info")
     datos = datos.find(class_="info-notaemol-porfecha")
@@ -79,78 +80,46 @@ def formatear_mega(url):
     print(pagina)
     return pagina
 
-def formatear_bio_bio(url):
+def formatear_bio_bio(url): #Faltan los comentarios
     pagina=dict()
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     #Sacar datos
-    titulo = soup.find(id="cuDetalle_cuTitular_tituloNoticia")
-    subtitulo = soup.find(id="cuDetalle_cuTitular_bajadaNoticia")
-    datos= soup.find(id="barra-agencias-info")
-    datos = datos.find(class_="info-notaemol-porfecha")
-    ############
-    #Arreglar el autor/fecha
-    datos_n = datos.text.strip()
+    titulo = soup.find(class_="nota-titular robotos").text.strip()
+    fecha_hora = soup.find(class_="nota-fecha am-hide").text.replace('\n','').replace('\r','').replace('\t','')
+    autor = soup.find(class_="autor-link").text.strip()
+    cuerpo = soup.find(class_="nota-contenido text-19 robotos").find_all("p")
+    cuerp =[]
+    for i in range(len(cuerpo)):
+        cuerp.append(cuerpo[i].text.replace('\n',''))
     ############
     #actualizar el diccionario
-    pagina.update({'titulo':titulo.text})
-    pagina.update({'subtitulo':subtitulo.text})
-    pagina.update({'autor y fecha':datos_n})
+    pagina.update({'titulo':titulo})
+    pagina.update({'Fecha y hora':fecha_hora})
+    pagina.update({'autor':autor})
+    pagina.update({'cuerpo':cuerp})
     ###########
-    cuerpo = soup.find(id="cuDetalle_cuTexto_textoNoticia")
-    cuerp = cuerpo.find_all('div')
-    cuerpo = []
-    for cue in cuerp:
-        noticias_relacionadas = cue.find('div',class_="flo_left cont_descrip_noticia")
-        noticias_relacionadas_2 = cue.find('article',class_="flo_left cont_nota_relacionada")
-        #print(cue.prettify())
-        if noticias_relacionadas == None or noticias_relacionadas_2 == None:
-
-            cuerpo.append(cue.text)
-    i=0
-    while i<len(cuerpo):
-        if(cuerpo[i]=='' or cuerpo[i]=='\n'):
-            cuerpo.remove(cuerpo[i])
-        #print(cuerpo[i])
-        i+=1
-    pagina.update({'cuerpo':cuerpo})
     print(pagina)
     return pagina
 
-def formatear_la_tercera(url):
+def formatear_la_tercera(url): #Faltan los comentarios
     pagina=dict()
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     #Sacar datos
-    titulo = soup.find(id="cuDetalle_cuTitular_tituloNoticia")
-    subtitulo = soup.find(id="cuDetalle_cuTitular_bajadaNoticia")
-    datos= soup.find(id="barra-agencias-info")
-    datos = datos.find(class_="info-notaemol-porfecha")
-    ############
-    #Arreglar el autor/fecha
-    datos_n = datos.text.strip()
+    titulo = soup.find('h1')
+    subtitulo = soup.find(class_="excerpt")
+    datos= soup.find(class_="author d-flex-center m-bot-10")
+    cuerpo = soup.find('div',class_="single-content").find_all('p',class_="paragraph")
+    cuerp=[]
+    for i in range(len(cuerpo)):
+        cuerp.append(cuerpo[i].text.strip('\n'))
     ############
     #actualizar el diccionario
     pagina.update({'titulo':titulo.text})
     pagina.update({'subtitulo':subtitulo.text})
-    pagina.update({'autor y fecha':datos_n})
+    pagina.update({'autor y fecha':datos.text})
+    pagina.update({'cuerpo':cuerp})
     ###########
-    cuerpo = soup.find(id="cuDetalle_cuTexto_textoNoticia")
-    cuerp = cuerpo.find_all('div')
-    cuerpo = []
-    for cue in cuerp:
-        noticias_relacionadas = cue.find('div',class_="flo_left cont_descrip_noticia")
-        noticias_relacionadas_2 = cue.find('article',class_="flo_left cont_nota_relacionada")
-        #print(cue.prettify())
-        if noticias_relacionadas == None or noticias_relacionadas_2 == None:
-
-            cuerpo.append(cue.text)
-    i=0
-    while i<len(cuerpo):
-        if(cuerpo[i]=='' or cuerpo[i]=='\n'):
-            cuerpo.remove(cuerpo[i])
-        #print(cuerpo[i])
-        i+=1
-    pagina.update({'cuerpo':cuerpo})
     print(pagina)
     return pagina
