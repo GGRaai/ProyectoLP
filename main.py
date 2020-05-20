6ba2ce01eea53c9eaa0dffb3ec0e9c65468c1444
GGRaai <raimundomoraga@hotmail.com>
2020-05-20 16:03:10 -0400

    Cambiado mega por publimetro, todo funcionando excepto emol/comentarios

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import noticias
 import test
 
-url = "https://www.biobiochile.cl/noticias/nacional/chile/2020/05/17/minsal-confirma-ministros-ward-briones-estan-cuerentena-preventiva-explica-caso-sichel.shtml"
+url = "https://www.publimetro.cl/cl/noticias/2020/05/20/desde-hoy-se-asumira-una-persona-desarrolla-sintomas-respiratorios-coronavirus.html"
 
 
 if __name__ == "__main__":
     if('emol' in url):
         test.formatear_emol(url)
-    elif('meganoticias' in url):
-        test.formatear_mega(url)
+    elif('publimetro' in url):
+        test.formatear_publimetro(url)
     elif('biobiochile' in url):
         test.formatear_bio_bio(url)
     elif('latercera' in url):
--- test.py
+++ test.py
@@ -42,41 +42,27 @@ def formatear_emol(url):
     print(pagina)
     return pagina
 
-def formatear_mega(url): #no funciona
+def formatear_publimetro(url): 
     pagina=dict()
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     #Sacar datos
-    titulo = soup.find('h1')
-    subtitulo = soup.find(id="cuDetalle_cuTitular_bajadaNoticia")
-    datos= soup.find(id="barra-agencias-info")
-    datos = datos.find(class_="info-notaemol-porfecha")
-    ############
-    #Arreglar el autor/fecha
-    datos_n = datos.text.strip()
+    titulo = soup.find('h1').text.strip()
+    subtitulo = soup.find(class_="excerpt").text.strip()
+    datos= soup.find(class_="autorandshare__autor").text.strip()
+    fecha = soup.find(class_="autorandshare__date").text.strip()
+    cuerpo = soup.find(class_="resumen").find_all("p")
+    cuerp =[]
+    for i in range(len(cuerpo)):
+        cuerp.append(cuerpo[i].text.strip())
     ############
     #actualizar el diccionario
-    pagina.update({'titulo':titulo.text})
-    pagina.update({'subtitulo':subtitulo.text})
-    pagina.update({'autor y fecha':datos_n})
+    pagina.update({'titulo':titulo})
+    pagina.update({'subtitulo':subtitulo})
+    pagina.update({'autor':datos})
+    pagina.update({'Fecha':fecha})
+    pagina.update({'cuerpo':cuerp})
     ###########
-    cuerpo = soup.find(id="cuDetalle_cuTexto_textoNoticia")
-    cuerp = cuerpo.find_all('div')
-    cuerpo = []
-    for cue in cuerp:
-        noticias_relacionadas = cue.find('div',class_="flo_left cont_descrip_noticia")
-        noticias_relacionadas_2 = cue.find('article',class_="flo_left cont_nota_relacionada")
-        #print(cue.prettify())
-        if noticias_relacionadas == None or noticias_relacionadas_2 == None:
-
-            cuerpo.append(cue.text)
-    i=0
-    while i<len(cuerpo):
-        if(cuerpo[i]=='' or cuerpo[i]=='\n'):
-            cuerpo.remove(cuerpo[i])
-        #print(cuerpo[i])
-        i+=1
-    pagina.update({'cuerpo':cuerpo})
     print(pagina)
     return pagina
 
