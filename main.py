a1eb106c96b7fe8b349a70002c9da7b38f0af775
GGRaai <raimundomoraga@hotmail.com>
2020-06-16 11:35:34 -0400

    analisis de tweets y noticias done, ver futuras mejoras

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
 import time
-
+import vader
 
 def sacar_paginas(url):
     driver.get(url)
@@ -48,16 +48,14 @@ if __name__ == "__main__":
             if("https://www.emol.com/noticias/Deportes" in link or "Grafico" in link):#Noticias de deporte generalmente son en vivos, Grafico son vivo, asi que no sirve
                 continue
             res = format.formatear_emol(link) #crea un diccionario con los datos de la noticia
-            objetos.append(noticias.Emol(res['titulo'],res['subtitulo'],res['autor y fecha'],res['autor y fecha'],res['cuerpo'],0,res['categoria'],res['subcategoria'])) #lo agrega a la lista
+            objetos.append(noticias.Emol(res['titulo'],res['subtitulo'],res['cuerpo'],res['categoria'])) #lo agrega a la lista
         elif('latercera' in link): #revisa si es noticia de la tercera
             if("https://www.latercera.com/encasa" in link):#manda a seccion en casa, no nos sirve
                 continue
             res = format.formatear_la_tercera(link)
-            objetos.append(noticias.La_Tercera(res['titulo'],res['subtitulo'],res['autor y fecha'],res['autor y fecha'],res['cuerpo'],0,res['categoria']))
+            objetos.append(noticias.La_Tercera(res['titulo'],res['subtitulo'],res['cuerpo'],res['categoria']))
     #loop de prueba, borrar en el producto final
-    for cont in objetos:
-        if(type(cont) is noticias.Emol):
-            print("Emol")
-        elif(type(cont) is noticias.La_Tercera):
-            print("La Tercera")
-        print(cont.titulo)
+    vader.sacar_valores_noticias(objetos)
+    vader.sacar_valores()
+    vader.graficos()
+    vader.graficos_n()
--- vader.py
+++ vader.py
@@ -1,5 +1,5 @@
 from tweet import *
-from classifier import *
+from classifier import SentimentClassifier
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -10,36 +10,91 @@ t_positivos = []#Lista que almacena todos los tweets positivos
 t_neutros = []#Lista que almacena los tweets neutros
 t_negativos = []#Lista que almacena los tweets negativos
 valores = {'Negativo':0,'Neutro':0,'Positivo':0}#Guarda los valores de cuantos tweets hay en cada categoria
+valores_n = {'Negativo':0,'Neutro':0,'Positivo':0}#Guarda los valores de cuantas noticias hay en cada categoria
 total = 0
+total_n = 0
+totales = {'Negativo':0,'Neutro':0,'Positivo':0}
+totales_n = {'Negativo':0,'Neutro':0,'Positivo':0}
 
-for trend in textos:
-    for tweet in textos[trend]:
-        a = clf.predict(tweet)
 
-        total +=1
-        if(a<0.3):
-            t_negativos.append(tweet)
-            valores['Negativo']+=1
-        elif(a>0.5):
-            t_positivos.append(tweet)
-            valores['Positivo']+=1
+def sacar_valores():
+    global total
+    global valores
+    global totales
+    for trend in textos:
+        for tweet in textos[trend]:
+            vals = clf.predict(tweet)
+            total +=1
+            if(vals<0.1):
+                t_negativos.append(tweet)
+                valores['Negativo']+=1
+                totales['Negativo']+=1
+            elif(vals>0.5):
+                t_positivos.append(tweet)
+                valores['Positivo']+=1
+                totales['Positivo']+=1
+            else:
+                t_neutros.append(tweet)
+                valores['Neutro']+=1
+                totales['Neutro']+=1
+
+    valores['Negativo'] = valores['Negativo']/total
+    valores['Neutro'] = valores['Neutro']/total
+    valores['Positivo'] = valores['Positivo']/total
+    print(valores)
+    print(totales)
+    print(total)
+
+def sacar_valores_noticias(lista):
+    global total_n
+    global valores_n
+    global totales_n
+    for noticia in lista:
+        for cuerpo in noticia.cuerpo:
+            vals = clf.predict(cuerpo)
+        total_n +=1
+        if(vals<0.1):
+            valores_n['Negativo']+=1
+            totales_n['Negativo']+=1
+        elif(vals>0.5):
+            valores_n['Positivo']+=1
+            totales_n['Positivo']+=1
         else:
-            t_neutros.append(tweet)
-            valores['Neutro']+=1
+            valores_n['Neutro']+=1
+            totales_n['Neutro']+=1
 
-valores['Negativo'] = valores['Negativo']/total
-valores['Neutro'] = valores['Neutro']/total
-valores['Positivo'] = valores['Positivo']/total
+    valores_n['Negativo'] = valores_n['Negativo']/total_n
+    valores_n['Neutro'] = valores_n['Neutro']/total_n
+    valores_n['Positivo'] = valores_n['Positivo']/total_n
+    print(valores_n)
+    print(totales_n)
+    print(total_n)
 
 
-objects = ('Negativo','Neutral','Positivo')
-y_pos = np.arange(len(objects))
-performance = [valores['Negativo'], valores['Neutro'], valores['Positivo']]
-barlist=plt.bar(y_pos, performance, align='center', alpha=0.5)
-barlist[0].set_color('red')
-barlist[1].set_color('blue')
-barlist[2].set_color('green')
-plt.xticks(y_pos, objects)
-plt.ylabel('Porcentaje')
-plt.title('Analisis de sentimientos')
-plt.show()
+def graficos():
+    global valores
+    objects = ('Negativo','Neutral','Positivo')
+    y_pos = np.arange(len(objects))
+    performance = [valores['Negativo'], valores['Neutro'], valores['Positivo']]
+    barlist=plt.bar(y_pos, performance, align='center', alpha=0.5)
+    barlist[0].set_color('red')
+    barlist[1].set_color('blue')
+    barlist[2].set_color('green')
+    plt.xticks(y_pos, objects)
+    plt.ylabel('Porcentaje')
+    plt.title('Analisis de sentimientos')
+    plt.show()
+
+def graficos_n():
+    global valores_n
+    objects = ('Negativo','Neutral','Positivo')
+    y_pos = np.arange(len(objects))
+    performance = [valores_n['Negativo'], valores_n['Neutro'], valores_n['Positivo']]
+    barlist=plt.bar(y_pos, performance, align='center', alpha=0.5)
+    barlist[0].set_color('red')
+    barlist[1].set_color('blue')
+    barlist[2].set_color('green')
+    plt.xticks(y_pos, objects)
+    plt.ylabel('Porcentaje')
+    plt.title('Analisis de sentimientos noticias')
+    plt.show()
